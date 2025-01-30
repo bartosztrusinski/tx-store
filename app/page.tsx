1a4f539a46a17cfb5fa28cc1fd46edc70e0641e7
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-01-31 00:26:31 +0100

    add initial logo and favicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import Image from 'next/image';
+
 export default function HomePage() {
-  return <div>Home Page</div>;
+  return (
+    <div className='flex items-center gap-1 p-2 bg-white text-black'>
+      <Image src='logo.svg' alt='tx store logo' width={32} height={32} />
+      <p className='text-2xl font-bold'>Store</p>
+    </div>
+  );
 }
