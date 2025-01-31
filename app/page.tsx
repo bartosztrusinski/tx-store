16ee0da2f6d2f642f73e5cd4ae1ff7ca990cf002
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-01-31 01:22:39 +0100

    add basic component classes

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function HomePage() {
   return (
     <div className='flex items-center gap-1 p-2 bg-white text-black'>
       <Image src='logo.svg' alt='tx store logo' width={32} height={32} />
-      <p className='text-2xl font-bold'>Store</p>
+      <p className='heading-2'>Store</p>
     </div>
   );
 }
