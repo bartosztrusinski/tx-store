882c394f51e41036bfe916000ddf5dd4ecb571dc
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-01-31 01:58:14 +0100

    add shadcn button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import Image from 'next/image';
 
+import { Button } from '@/components/ui/button';
+
 export default function HomePage() {
   return (
-    <div className='flex items-center gap-1 p-2 bg-white text-black'>
-      <Image src='logo.svg' alt='tx store logo' width={32} height={32} />
-      <p className='heading-2'>Store</p>
-    </div>
+    <>
+      <div className='flex items-center gap-1 p-2 bg-white text-black'>
+        <Image src='logo.svg' alt='tx store logo' width={32} height={32} />
+        <p>Store</p>
+      </div>
+      <div className='flex-center p-4'>
+        <Button>Get Started</Button>
+      </div>
+    </>
   );
 }
