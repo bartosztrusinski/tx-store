5a49086497c2e749dc1dce72bebd26f8013aa810
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-02-08 17:40:03 +0100

    create separate folder for images

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,13 +3,12 @@ import Image from 'next/image';
 
 import { Button } from '@/components/ui/button';
 
-import logo from '@/public/logo.svg';
 import { APP_NAME } from '@/lib/constants';
 
 export default function NotFound() {
   return (
     <div className='flex-center h-screen flex-col gap-4 p-10 text-center'>
-      <Image src={logo} alt={`${APP_NAME} logo`} width={48} height={48} />
+      <Image src='images/logo.svg' alt={`${APP_NAME} logo`} width={48} height={48} />
       <div>
         <h1 className='heading-2'>Not Found</h1>
         <p className='mt-1 lg:text-lg'>Looks like this page got lost in the fashion jungle</p>
--- components/header/index.tsx
+++ components/header/index.tsx
@@ -8,7 +8,6 @@ import { Input } from '@/components/ui/input';
 import { CategoryDrawer } from './category-drawer';
 import { Menu } from './menu';
 
-import logo from '@/public/logo.svg';
 import { APP_NAME } from '@/lib/constants';
 
 export function Header() {
@@ -18,7 +17,7 @@ export function Header() {
         <section className='flex-center shrink-0 gap-3 md:gap-6'>
           <CategoryDrawer />
           <Link href='/' className='flex-center gap-1'>
-            <Image src={logo} alt={`${APP_NAME} logo`} width={32} height={32} priority />
+            <Image src='images/logo.svg' alt={`${APP_NAME} logo`} width={32} height={32} priority />
             <p className='heading-1 hidden md:block'>Store</p>
           </Link>
         </section>
