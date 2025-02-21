88372cae809316fcbd6005c6919f949cf6571feb
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-02-21 23:02:51 +0100

    add navigation logic to thumbs and dots

--- components/product/product-image-gallery.tsx
+++ components/product/product-image-gallery.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import Image from 'next/image';
+import { useEffect, useRef, useState } from 'react';
 import type { Product } from '@prisma/client';
 
 import {
@@ -7,7 +10,10 @@ import {
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
+  type CarouselApi,
 } from '@/components/ui/carousel';
+
+import { cn } from '@/lib/utils';
 
 type Props = {
   images: Product['images'];
@@ -15,16 +21,51 @@ type Props = {
 };
 
 export function ProductImageGallery({ images, alt }: Props) {
+  const [carouselApi, setCarouselApi] = useState<CarouselApi>();
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const thumbnailsRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!carouselApi) return;
+
+    const controller = new AbortController();
+
+    setCurrentImageIndex(carouselApi.selectedScrollSnap());
+
+    carouselApi.on('select', () => {
+      setCurrentImageIndex(carouselApi.selectedScrollSnap());
+    });
+
+    window.addEventListener(
+      'resize',
+      () => {
+        if (thumbnailsRef.current) {
+          thumbnailsRef.current.style.height = `${carouselApi.rootNode().clientHeight}px`;
+        }
+      },
+      { signal: controller.signal },
+    );
+
+    return () => {
+      controller.abort();
+    };
+  }, [carouselApi]);
+
   return (
-    <div className='flex flex-col gap-4 md:flex-row'>
-      {/* TODO add thumbnail logic */}
-      <div className='order-2 flex shrink-0 gap-2 md:order-none md:flex-col'>
+    <div className='flex flex-col-reverse gap-4 md:flex-row'>
+      <div ref={thumbnailsRef} className='flex max-h-max shrink-0 gap-2 overflow-auto md:flex-col'>
         {images.map((image, index) => (
-          <Image key={index} src={image} alt={alt} width={60} height={60} className='rounded' />
+          <button
+            key={index}
+            onClick={() => carouselApi?.scrollTo(index)}
+            className={cn('shrink-0 ring-2', index === currentImageIndex && 'opacity-80')}
+          >
+            <Image src={image} alt={alt} width={60} height={60} className='rounded' />
+          </button>
         ))}
       </div>
 
-      <Carousel>
+      <Carousel setApi={setCarouselApi}>
         <CarouselContent>
           {images.map((image, index) => (
             <CarouselItem key={index}>
@@ -38,6 +79,18 @@ export function ProductImageGallery({ images, alt }: Props) {
             </CarouselItem>
           ))}
         </CarouselContent>
+        <div className='flex-center absolute inset-x-0 bottom-2 gap-2'>
+          {images.map((_, index) => (
+            <button
+              key={index}
+              onClick={() => carouselApi?.scrollTo(index)}
+              className={cn(
+                'size-2 rounded-full bg-white ring-1 ring-black/25',
+                index === currentImageIndex && 'bg-black',
+              )}
+            />
+          ))}
+        </div>
         <CarouselPrevious className='inset-auto bottom-0 right-14' />
         <CarouselNext className='inset-auto bottom-0 right-4' />
       </Carousel>
