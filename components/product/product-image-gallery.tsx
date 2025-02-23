e2070b26627c5e1e3082d985765b514aa70a574d
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-02-23 01:18:45 +0100

    use carousel for thumbs

--- components/product/product-image-gallery.tsx
+++ components/product/product-image-gallery.tsx
@@ -21,14 +21,40 @@ type Props = {
 };
 
 export function ProductImageGallery({ images, alt }: Props) {
-  const [carouselApi, setCarouselApi] = useState<CarouselApi>();
+  const [mainCarouselApi, setMainCarouselApi] = useState<CarouselApi>();
+  const [thumbCarouselApi, setThumbCarouselApi] = useState<CarouselApi>();
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const onThumbClick = useCallback(
+    (index: number) => {
+      if (!mainCarouselApi || !thumbCarouselApi) return;
+      mainCarouselApi.scrollTo(index);
+    },
+    [mainCarouselApi, thumbCarouselApi],
+  );
+
+  const onMainCarouselSelect = useCallback(() => {
+    if (!mainCarouselApi || !thumbCarouselApi) return;
+
+    const currentImageIndex = mainCarouselApi.selectedScrollSnap();
+
+    setCurrentImageIndex(currentImageIndex);
+    thumbCarouselApi.scrollTo(currentImageIndex);
+  }, [mainCarouselApi, thumbCarouselApi]);
+
+  useEffect(() => {
+    if (!mainCarouselApi) return;
+
+    onMainCarouselSelect();
+
+    mainCarouselApi.on('select', onMainCarouselSelect).on('reInit', onMainCarouselSelect);
+  }, [mainCarouselApi, onMainCarouselSelect]);
 
   const thumbContainerRef = useCallback(
     (thumbsNode: HTMLElement | null) => {
-      if (!thumbsNode || !carouselApi) return;
+      if (!thumbsNode || !mainCarouselApi) return;
 
-      const carouselNode = carouselApi.rootNode();
+      const carouselNode = mainCarouselApi.rootNode();
       const resizeObserver = new ResizeObserver(() => {
         thumbsNode.style.height = `${carouselNode.getBoundingClientRect().height}px`;
       });
@@ -36,37 +62,36 @@ export function ProductImageGallery({ images, alt }: Props) {
       resizeObserver.observe(carouselNode);
       return () => resizeObserver.disconnect();
     },
-    [carouselApi],
+    [mainCarouselApi],
   );
-
-  useEffect(() => {
-    if (!carouselApi) return;
-
-    setCurrentImageIndex(carouselApi.selectedScrollSnap());
-
-    carouselApi.on('select', () => {
-      setCurrentImageIndex(carouselApi.selectedScrollSnap());
-    });
-  }, [carouselApi]);
 
   return (
     <div className='flex flex-col-reverse gap-4 md:flex-row'>
-      <div
-        ref={thumbContainerRef}
-        className='flex max-h-max shrink-0 gap-2 overflow-auto md:flex-col'
+      <Carousel
+        className='max-h-max shrink-0'
+        orientation='vertical'
+        opts={{
+          containScroll: 'keepSnaps',
+          dragFree: true,
+        }}
+        setApi={setThumbCarouselApi}
       >
-        {images.map((image, index) => (
-          <button
-            key={index}
-            onClick={() => carouselApi?.scrollTo(index)}
-            className={cn('shrink-0 ring-2', index === currentImageIndex && 'opacity-80')}
-          >
-            <Image src={image} alt={alt} width={60} height={60} className='rounded' />
-          </button>
-        ))}
-      </div>
+        <CarouselContent ref={thumbContainerRef} className='-mt-2 mb-2'>
+          {images.map((image, index) => (
+            <CarouselItem key={index} className='basis-0 pt-2'>
+              <button
+                type='button'
+                onClick={() => onThumbClick(index)}
+                className={cn('rounded', index === currentImageIndex && 'brightness-75 filter')}
+              >
+                <Image src={image} alt={alt} width={60} height={60} className='rounded' />
+              </button>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
 
-      <Carousel setApi={setCarouselApi}>
+      <Carousel setApi={setMainCarouselApi}>
         <CarouselContent>
           {images.map((image, index) => (
             <CarouselItem key={index}>
@@ -84,7 +109,7 @@ export function ProductImageGallery({ images, alt }: Props) {
           {images.map((_, index) => (
             <button
               key={index}
-              onClick={() => carouselApi?.scrollTo(index)}
+              onClick={() => onThumbClick(index)}
               className={cn(
                 'size-2 rounded-full bg-white ring-1 ring-black/25',
                 index === currentImageIndex && 'bg-black',
