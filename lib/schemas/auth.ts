5422de4d07261aeb6f34b5ee96672c3e6cd86c5f
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-03-06 21:34:31 +0100

    create base auth schemas

---
+++ lib/schemas/auth.ts
@@ -0,0 +1,44 @@
+import { z } from 'zod';
+
+const LoginSchema = z.object({
+  email: z.string().email({
+    message: 'Please enter a valid email address',
+  }),
+  password: z.string().min(1, {
+    message: 'Please enter your password',
+  }),
+});
+
+const RegisterSchema = z
+  .object({
+    name: z.string().min(3, {
+      message: 'Name must be at least 3 characters long',
+    }),
+    email: z.string().email({
+      message: 'Please enter a valid email address',
+    }),
+    password: z
+      .string()
+      .min(8, {
+        message: 'Password must be at least 8 characters long',
+      })
+      .regex(/[a-z]/, {
+        message: 'Password must contain at least one lowercase letter',
+      })
+      .regex(/[A-Z]/, {
+        message: 'Password must contain at least one uppercase letter',
+      })
+      .regex(/[0-9]/, {
+        message: 'Password must contain at least one number',
+      })
+      .regex(/[!@#$%^&*()_+]/, {
+        message: 'Password must contain at least one special character',
+      }),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: 'Passwords do not match',
+    path: ['confirmPassword'],
+  });
+
+export { LoginSchema, RegisterSchema };
