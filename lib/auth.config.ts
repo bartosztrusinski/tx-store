576839e49c2b7f247ad42f7d97a4c7f288356c30
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-03-11 13:17:31 +0100

    add logic for authentication endpoint

--- lib/auth.config.ts
+++ lib/auth.config.ts
@@ -1,6 +1,40 @@
+import bcrypt from 'bcryptjs';
 import Credentials from 'next-auth/providers/credentials';
 import type { NextAuthConfig } from 'next-auth';
 
+import { prisma } from '@/lib/prisma';
+import { LoginSchema } from '@/lib/schemas/auth';
+
 export default {
-  providers: [Credentials],
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const validationResult = LoginSchema.safeParse(credentials);
+
+        if (!validationResult.success) {
+          return null;
+        }
+
+        const { email, password } = validationResult.data;
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email,
+          },
+        });
+
+        if (!user || !user.password) {
+          return null;
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordCorrect) {
+          return null;
+        }
+
+        return user;
+      },
+    }),
+  ],
 } satisfies NextAuthConfig;
