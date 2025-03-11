ed696fd8ffa6ce05d630898028c1261905c8c57e
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-03-11 13:24:57 +0100

    add logic for auth server actions

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -1,10 +1,15 @@
 'use server';
 
+import bcrypt from 'bcryptjs';
+import { AuthError } from 'next-auth';
+
+import { prisma } from '@/lib/prisma';
+import { signIn } from '@/lib/auth';
 import { LoginSchema, RegisterSchema } from '@/lib/schemas/auth';
 import type { ActionResponse } from '@/lib/types';
 
 export async function login(
-  prevState: ActionResponse<typeof LoginSchema>,
+  state: ActionResponse<typeof LoginSchema>,
   formData: FormData,
 ): Promise<ActionResponse<typeof LoginSchema>> {
   const data = {
@@ -17,11 +22,36 @@ export async function login(
   if (!validationResult.success) {
     return {
       isSuccess: false,
+      message: validationResult.error.flatten().formErrors[0],
       errors: validationResult.error.flatten().fieldErrors,
     };
   }
 
-  // Perform login logic here
+  const { email, password } = validationResult.data;
+
+  try {
+    await signIn('credentials', {
+      email,
+      password,
+      redirectTo: '/',
+    });
+  } catch (error) {
+    if (!(error instanceof AuthError)) {
+      throw error;
+    }
+
+    if (error.type === 'CredentialsSignin') {
+      return {
+        isSuccess: false,
+        message: 'Please enter correct credentials',
+      };
+    }
+
+    return {
+      isSuccess: false,
+      message: 'An error occurred while logging in. Please try again later.',
+    };
+  }
 
   return {
     isSuccess: true,
@@ -30,7 +60,7 @@ export async function login(
 }
 
 export async function register(
-  prevState: ActionResponse<typeof RegisterSchema>,
+  state: ActionResponse<typeof RegisterSchema>,
   formData: FormData,
 ): Promise<ActionResponse<typeof RegisterSchema>> {
   const data = {
@@ -43,15 +73,41 @@ export async function register(
   const validationResult = RegisterSchema.safeParse(data);
 
   if (!validationResult.success) {
-    console.dir(validationResult.error);
-
     return {
       isSuccess: false,
+      message: validationResult.error.flatten().formErrors[0],
       errors: validationResult.error.flatten().fieldErrors,
     };
   }
 
-  // Perform registration logic here
+  const { email, name, password } = validationResult.data;
+
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      email: validationResult.data.email,
+    },
+  });
+
+  if (existingUser) {
+    return {
+      isSuccess: false,
+      errors: {
+        email: ['Email already in use'],
+      },
+    };
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  await prisma.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO Email verification
 
   return {
     isSuccess: true,
