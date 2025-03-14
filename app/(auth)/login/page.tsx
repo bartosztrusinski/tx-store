d08d329d6863f95f2bd0d617804bfd8c6f150a53
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-03-14 23:10:11 +0100

    add links to home page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { ArrowLeftCircle } from 'lucide-react';
 
 import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card';
 import { LoginForm } from '@/components/auth/login-form';
@@ -7,9 +8,15 @@ import { APP_NAME } from '@/lib/constants';
 
 export default function LoginPage() {
   return (
-    <Card className='basis-full space-y-2'>
-      <CardHeader className='flex-center gap-1'>
-        <Image src='/images/logo.svg' alt={`${APP_NAME} logo`} width={36} height={36} />
+    <Card className='relative basis-full space-y-2'>
+      <Link href='/' className='flex-center absolute left-1 top-1 gap-1 p-2 text-sm'>
+        <ArrowLeftCircle className='size-5' />
+        Back
+      </Link>
+      <CardHeader className='flex-center'>
+        <Link href='/' className='p-0.5'>
+          <Image src='/images/logo.svg' alt={`${APP_NAME} logo`} width={36} height={36} />
+        </Link>
         <h1 className='heading-2'>Log In</h1>
         <p className='text-pretty text-center text-muted-foreground'>
           Sign in to your account to continue
--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { ArrowLeftCircle } from 'lucide-react';
 
 import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card';
 import { RegisterForm } from '@/components/auth/register-form';
@@ -7,9 +8,15 @@ import { APP_NAME } from '@/lib/constants';
 
 export default function RegisterPage() {
   return (
-    <Card className='basis-full space-y-2'>
+    <Card className='relative basis-full space-y-2'>
+      <Link href='/' className='flex-center absolute left-1 top-1 gap-1 p-2 text-sm'>
+        <ArrowLeftCircle className='size-5' />
+        Back
+      </Link>
       <CardHeader className='flex-center gap-1'>
-        <Image src='/images/logo.svg' alt={`${APP_NAME} logo`} width={36} height={36} />
+        <Link href='/' className='p-0.5'>
+          <Image src='/images/logo.svg' alt={`${APP_NAME} logo`} width={36} height={36} />
+        </Link>
         <h1 className='heading-2'>Sign Up</h1>
         <p className='text-pretty text-center text-muted-foreground'>
           Create an account to get started with {APP_NAME}
