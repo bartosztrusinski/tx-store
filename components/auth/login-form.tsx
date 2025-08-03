a9c7bf75efe8779a67deb730d56b9b0a85fef19d
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-08-03 23:46:35 +0200

    add form autocomplete and group inputs

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,26 +1,31 @@
 'use client';
 
 import { useActionState } from 'react';
-
+import { login } from '@/lib/actions/auth';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { SuccessAlert } from '@/components/success-alert';
 import { ErrorAlert } from '@/components/error-alert';
-import { login } from '@/lib/actions/auth';
-
 import { FieldErrors } from './field-errors';
 
 export function LoginForm() {
-  const [state, action, isPending] = useActionState(login, {
-    isSuccess: false,
-  });
+  const [state, action, isPending] = useActionState(login, { isSuccess: false });
 
   return (
     <form action={action} className='flex flex-col gap-3'>
-      <Input type='email' name='email' placeholder='Email' />
-      <FieldErrors errors={state?.errors?.email} />
-      <Input type='password' name='password' placeholder='Password' />
-      <FieldErrors errors={state?.errors?.password} />
+      <div>
+        <Input type='email' name='email' placeholder='Email' autoComplete='email' />
+        <FieldErrors errors={state?.errors?.email} />
+      </div>
+      <div>
+        <Input
+          type='password'
+          name='password'
+          placeholder='Password'
+          autoComplete='current-password'
+        />
+        <FieldErrors errors={state?.errors?.password} />
+      </div>
 
       {state?.message &&
         (state.isSuccess ?
--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -1,30 +1,39 @@
 'use client';
 
 import { useActionState } from 'react';
-
+import { register } from '@/lib/actions/auth';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { SuccessAlert } from '@/components/success-alert';
 import { ErrorAlert } from '@/components/error-alert';
-import { register } from '@/lib/actions/auth';
-
 import { FieldErrors } from './field-errors';
 
 export function RegisterForm() {
-  const [state, action, isPending] = useActionState(register, {
-    isSuccess: false,
-  });
+  const [state, action, isPending] = useActionState(register, { isSuccess: false });
 
   return (
     <form action={action} className='flex flex-col gap-3'>
-      <Input name='name' placeholder='Name' />
-      <FieldErrors errors={state.errors?.name} />
-      <Input type='email' name='email' placeholder='Email' />
-      <FieldErrors errors={state.errors?.email} />
-      <Input type='password' name='password' placeholder='Password' />
-      <FieldErrors errors={state.errors?.password} />
-      <Input type='password' name='confirmPassword' placeholder='Repeat Password' />
-      <FieldErrors errors={state.errors?.confirmPassword} />
+      <div>
+        <Input type='email' name='email' placeholder='Email' autoComplete='email' />
+        <FieldErrors errors={state.errors?.email} />
+      </div>
+      <div>
+        <Input name='name' placeholder='Name' autoComplete='name' />
+        <FieldErrors errors={state.errors?.name} />
+      </div>
+      <div>
+        <Input type='password' name='password' placeholder='Password' autoComplete='new-password' />
+        <FieldErrors errors={state.errors?.password} />
+      </div>
+      <div>
+        <Input
+          type='password'
+          name='confirmPassword'
+          placeholder='Repeat Password'
+          autoComplete='new-password'
+        />
+        <FieldErrors errors={state.errors?.confirmPassword} />
+      </div>
 
       {state?.message &&
         (state.isSuccess ?
