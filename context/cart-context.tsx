8f13c1dc063776b7e19122af5ceff66696851e86
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-08-09 01:36:24 +0200

    change cart context setters

--- context/cart-context.tsx
+++ context/cart-context.tsx
@@ -10,59 +10,19 @@ type CartItem = {
 
 type CartContextType = {
   cartItems: CartItem[];
-  addItem: (id: CartItem['id']) => void;
   removeItem: (id: CartItem['id']) => void;
   incrementItemQuantity: (id: CartItem['id']) => void;
   decrementItemQuantity: (id: CartItem['id']) => void;
+  setItemQuantity: (id: CartItem['id'], quantity: number) => void;
 };
 
 const CartContext = createContext<CartContextType>({
   cartItems: [],
-  addItem: () => {},
   removeItem: () => {},
   incrementItemQuantity: () => {},
   decrementItemQuantity: () => {},
+  setItemQuantity: () => {},
 });
-
-export function CartProvider({ children }: { children: ReactNode }) {
-  const [cartItems, setCartItems] = useState<CartItem[]>([]);
-
-  const addItem = (id: CartItem['id']) => {
-    setCartItems((prevItems) => [...prevItems, { id, quantity: 1 }]);
-  };
-
-  const removeItem = (id: CartItem['id']) => {
-    setCartItems((prevItems) => prevItems.filter((item) => item.id !== id));
-  };
-
-  const incrementItemQuantity = (id: CartItem['id']) => {
-    setCartItems((prevItems) =>
-      prevItems.map((item) => (item.id === id ? { ...item, quantity: item.quantity + 1 } : item)),
-    );
-  };
-
-  const decrementItemQuantity = (id: CartItem['id']) => {
-    setCartItems((prevItems) =>
-      prevItems.map((item) =>
-        item.id === id ? { ...item, quantity: Math.max(item.quantity - 1, 0) } : item,
-      ),
-    );
-  };
-
-  return (
-    <CartContext.Provider
-      value={{
-        cartItems,
-        addItem,
-        removeItem,
-        incrementItemQuantity,
-        decrementItemQuantity,
-      }}
-    >
-      {children}
-    </CartContext.Provider>
-  );
-}
 
 export function useCart() {
   const context = useContext(CartContext);
@@ -72,4 +32,58 @@ export function useCart() {
   }
 
   return context;
+}
+
+export function CartProvider({ children }: { children: ReactNode }) {
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  const incrementItemQuantity = (id: CartItem['id']) => {
+    const currentQuantity = cartItems.find((item) => item.id === id)?.quantity;
+
+    setCartItems((prevItems) =>
+      currentQuantity ?
+        prevItems.map((item) =>
+          item.id === id ? { ...item, quantity: currentQuantity + 1 } : item,
+        )
+      : [...prevItems, { id, quantity: 1 }],
+    );
+  };
+
+  const decrementItemQuantity = (id: CartItem['id']) => {
+    const currentQuantity = cartItems.find((item) => item.id === id)?.quantity;
+
+    setCartItems((prevItems) =>
+      currentQuantity === 1 ?
+        prevItems.filter((item) => item.id !== id)
+      : prevItems.map((item) =>
+          item.id === id ? { ...item, quantity: Math.max(item.quantity - 1, 0) } : item,
+        ),
+    );
+  };
+
+  const setItemQuantity = (id: CartItem['id'], quantity: number) => {
+    setCartItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === id ? { ...item, quantity: Math.max(0, quantity) } : item,
+      ),
+    );
+  };
+
+  const removeItem = (id: CartItem['id']) => {
+    setCartItems((prevItems) => prevItems.filter((item) => item.id !== id));
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        cartItems,
+        incrementItemQuantity,
+        decrementItemQuantity,
+        removeItem,
+        setItemQuantity,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
 }
