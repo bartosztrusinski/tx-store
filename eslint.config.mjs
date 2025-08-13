05df7eaf5e1a4656eec0bb8264fff311798199df
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-08-14 00:37:11 +0200

    add eslint plugin for sorting; enforce consistent type imports; upgrade packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,18 @@
 import { FlatCompat } from '@eslint/eslintrc';
+import perfectionist from 'eslint-plugin-perfectionist';
 
 const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
 });
 
-const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier')];
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  perfectionist.configs['recommended-natural'],
+  {
+    rules: {
+      '@typescript-eslint/consistent-type-imports': ['error', { fixStyle: 'inline-type-imports' }],
+    },
+  },
+];
 
 export default eslintConfig;
