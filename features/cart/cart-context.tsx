da0c35ba139b27a31ad90d393b6c72d83d9def45
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-08-18 23:55:39 +0200

    add initial cart persistence in local storage

--- features/cart/cart-context.tsx
+++ features/cart/cart-context.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { type Product } from '@prisma/client';
-import { createContext, type ReactNode, useContext, useReducer } from 'react';
+import { createContext, type ReactNode, useContext, useEffect, useReducer } from 'react';
 
 type Action =
   | { payload: { id: Product['id'] }; type: 'cart/incrementItemQuantity' }
@@ -10,6 +10,8 @@ type Action =
   | { payload: { id: Product['id']; quantity: number }; type: 'cart/setItemQuantity' };
 type Dispatch = (action: Action) => void;
 type State = Record<Product['id'], { quantity: number }>;
+
+const CART_STORAGE_KEY = 'cart';
 
 function cartReducer(state: State, action: Action): State {
   switch (action.type) {
@@ -62,8 +64,22 @@ function cartReducer(state: State, action: Action): State {
 const CartContext = createContext<{ cart: State; dispatch: Dispatch } | null>(null);
 
 export function CartProvider({ children }: { children: ReactNode }) {
-  const [cart, dispatch] = useReducer(cartReducer, {});
+  const [cart, dispatch] = useReducer(cartReducer, {}, (initialCart) => {
+    try {
+      const storedCart = localStorage.getItem(CART_STORAGE_KEY);
+      return storedCart ? JSON.parse(storedCart) : initialCart;
+    } catch (error) {
+      console.error(`Failed to get local cart: ${error}`);
+    }
+
+    return initialCart;
+  });
   const value = { cart, dispatch };
+
+  useEffect(() => {
+    localStorage.setItem(CART_STORAGE_KEY, JSON.stringify(cart));
+  }, [cart]);
+
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 }
 
