04cc04ac75bb2c7ddf11fedac7a2fc1ccbe8dbb6
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-08-30 19:45:11 +0200

    add logic to setCartItem action -  handle guest cart and cart creation

--- features/cart/actions.ts
+++ features/cart/actions.ts
@@ -1,56 +1,94 @@
 'use server';
 
+import { type Cart } from '@prisma/client';
 import { revalidatePath } from 'next/cache';
-import { headers } from 'next/headers';
+import { cookies, headers } from 'next/headers';
+import { randomUUID } from 'node:crypto';
 
 import { auth } from '@/lib/auth';
 import { prisma } from '@/lib/prisma';
 
 export async function setCartItem(productId: number, quantity: number) {
-  const session = await auth.api.getSession({ headers: await headers() });
+  const cartId = await getCartId();
 
-  if (!session) {
+  if (!cartId) {
     return {
-      message: 'User not authenticated',
+      message: 'Cart not found',
       success: false,
     };
   }
 
-  revalidatePath('/products');
+  revalidatePath('/');
 
   if (quantity === 0) {
     await prisma.cartItem.delete({
-      where: {
-        userId_productId: {
-          productId,
-          userId: session.user.id,
-        },
-      },
+      where: { cartId_productId: { cartId, productId } },
     });
 
-    return {
-      success: true,
-    };
+    return { success: true };
   }
 
   await prisma.cartItem.upsert({
-    create: {
-      productId,
-      quantity,
-      userId: session.user.id,
-    },
-    update: {
-      quantity,
-    },
-    where: {
-      userId_productId: {
-        productId,
-        userId: session.user.id,
-      },
-    },
+    create: { cartId, productId, quantity },
+    update: { quantity },
+    where: { cartId_productId: { cartId, productId } },
   });
 
-  return {
-    success: true,
-  };
+  return { success: true };
+}
+
+async function createGuestCart(): Promise<Cart['id']> {
+  const sessionId = randomUUID();
+  const newCart = await prisma.cart.create({
+    data: { sessionId },
+  });
+
+  cookieStore.set('cartId', sessionId);
+
+  return newCart.id;
+}
+
+async function createUserCart(userId: Cart['userId']): Promise<Cart['id']> {
+  const newCart = await prisma.cart.create({
+    data: { userId },
+  });
+
+  return newCart.id;
+}
+
+async function findGuestCartId(sessionId: string): Promise<Cart['id'] | null> {
+  const cart = await prisma.cart.findUnique({
+    select: { id: true },
+    where: { sessionId },
+  });
+
+  return cart?.id ?? null;
+}
+
+async function findUserCartId(userId: string): Promise<Cart['id'] | null> {
+  const cart = await prisma.cart.findUnique({
+    select: { id: true },
+    where: { userId },
+  });
+
+  return cart?.id ?? null;
+}
+
+async function getCartId(): Promise<Cart['id'] | null> {
+  const session = await auth.api.getSession({ headers: await headers() });
+  const userId = session?.user.id;
+  const cookieStore = await cookies();
+  const sessionId = cookieStore.get('cartId')?.value;
+
+  if (userId) {
+    const cartId = await findUserCartId(userId);
+    return cartId ?? (await createUserCart(userId));
+  }
+
+  if (sessionId) {
+    const cartId = await findGuestCartId(sessionId);
+    return cartId ?? (await createGuestCart());
+  }
+
+  return null;
 }
