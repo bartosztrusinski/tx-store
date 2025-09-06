29390f842ddfa9f7ee3d22bbedfb73b81707590a
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-09-06 21:54:37 +0200

    create dir for utils

--- components/product/product-image-gallery.tsx
+++ components/product/product-image-gallery.tsx
@@ -14,7 +14,7 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from '@/components/ui/carousel';
-import { cn } from '@/lib/utils';
+import { cn } from '@/lib/utils/cn';
 
 type Props = {
   alt: string;
