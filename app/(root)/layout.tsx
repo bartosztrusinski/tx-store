b18ab9ad3b637eeae342fcf43450189e29ae9f34
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-09-20 19:44:13 +0200

    delete cart context

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,11 @@
 import { Footer } from '@/components/footer';
 import { Header } from '@/components/header';
-import { CartProvider } from '@/features/cart/cart-context';
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <div className='flex min-h-screen flex-col font-sans'>
-      <CartProvider>
-        <Header />
-        <main className='wrapper flex-1'>{children}</main>
-      </CartProvider>
+      <Header />
+      <main className='wrapper flex-1'>{children}</main>
       <Footer />
     </div>
   );
