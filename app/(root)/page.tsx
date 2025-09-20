0716921e4c868c4a011324911a31746d3d8a0e89
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-09-20 19:47:28 +0200

    remove title prop from list component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,11 +3,11 @@ import { getLatestProducts } from '@/features/product/data';
 
 export default async function HomePage() {
   const latestProducts = await getLatestProducts();
-  const title = <h1 className='heading-2 mb-4 font-bold'>Newest Arrivals</h1>;
 
   return (
     <>
-      <ProductList limit={4} products={latestProducts} title={title} />
+      <h1 className='heading-2 mb-4 font-bold'>Newest Arrivals</h1>
+      <ProductList limit={4} products={latestProducts} />
     </>
   );
 }
--- components/product/product-list.tsx
+++ components/product/product-list.tsx
@@ -1,29 +1,24 @@
 import { type Product } from '@prisma/client';
-import { type ReactNode } from 'react';
 
 import { ProductCard } from './product-card';
 
 type Props = {
   limit?: number;
   products: Product[];
-  title?: ReactNode;
 };
 
-export function ProductList({ limit, products, title }: Props) {
+export function ProductList({ limit, products }: Props) {
   const limitedProducts = limit ? products.toSpliced(limit) : products;
 
   return (
-    <div>
-      {title}
-      <ul className='grid grid-cols-2 gap-3 sm:grid-cols-2 md:grid-cols-3 md:gap-4 lg:grid-cols-4'>
-        {limitedProducts.length > 0 ?
-          limitedProducts.map((product) => (
-            <li key={product.slug}>
-              <ProductCard product={product} />
-            </li>
-          ))
-        : <p>No products found</p>}
-      </ul>
-    </div>
+    <ul className='grid grid-cols-2 gap-3 sm:grid-cols-2 md:grid-cols-3 md:gap-4 lg:grid-cols-4'>
+      {limitedProducts.length > 0 ?
+        limitedProducts.map((product) => (
+          <li key={product.slug}>
+            <ProductCard product={product} />
+          </li>
+        ))
+      : <p>No products found</p>}
+    </ul>
   );
 }
