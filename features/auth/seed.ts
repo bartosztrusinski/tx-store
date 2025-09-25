b5c29ed2443cf7354700ead734d7c3ab02f18715
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-09-25 17:17:47 +0200

    move seed data to features dir

---
+++ features/auth/seed.ts
@@ -0,0 +1,17 @@
+export const users: { email: string; name: string; password: string }[] = [
+  {
+    email: 'marco@polo.com',
+    name: 'Marco Polo',
+    password: 'password123',
+  },
+  {
+    email: 'jane.smith@example.com',
+    name: 'Jane Smith',
+    password: 'password123',
+  },
+  {
+    email: 'john.doe@example.com',
+    name: 'John Doe',
+    password: 'password123',
+  },
+];
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,21 +1,26 @@
 import { PrismaClient } from '@prisma/client';
 
+import { users } from '@/features/auth/seed';
+import { products } from '@/features/product/seed';
 import { auth } from '@/lib/auth';
-import { sampleData } from '@/lib/sample-data';
 
 seed();
 
+async function clearDatabase(prisma: PrismaClient) {
+  await prisma.user.deleteMany();
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verification.deleteMany();
+  await prisma.product.deleteMany();
+  await prisma.cart.deleteMany();
+  await prisma.cartItem.deleteMany();
+}
+
 async function seed() {
   const prisma = new PrismaClient();
-  const { products, users } = sampleData;
 
   try {
-    await prisma.product.deleteMany();
-    await prisma.account.deleteMany();
-    await prisma.session.deleteMany();
-    await prisma.user.deleteMany();
-    await prisma.verification.deleteMany();
-
+    await clearDatabase(prisma);
     await prisma.product.createMany({ data: products });
     await Promise.all(users.map((user) => auth.api.signUpEmail({ body: user })));
   } catch (error) {
