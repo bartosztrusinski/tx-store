8c518f4dc398867557276a5f6b1ec860bf73426a
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-09-25 14:52:56 +0200

    use cart data queries

--- features/cart/actions.ts
+++ features/cart/actions.ts
@@ -1,104 +1,76 @@
 'use server';
 
-import { type Cart, type CartItem, Prisma } from '@prisma/client';
+import { type Cart, type CartItem } from '@prisma/client';
 import { revalidatePath } from 'next/cache';
-import { cookies, headers } from 'next/headers';
+import { headers } from 'next/headers';
 import { randomUUID } from 'node:crypto';
 
-import { getCartId } from '@/features/cart/data';
+import { deleteCartCookie, getCartCookie, setCartCookie } from '@/features/cart/cookie';
+import {
+  createGuestCart,
+  createOrGetUserCartWithItems,
+  createUserCart,
+  deleteCart,
+  deleteCartItem,
+  getCartId,
+  getGuestCartWithItems,
+  upsertCartItem,
+  upsertCartItems,
+} from '@/features/cart/data';
+import { getProductStock } from '@/features/product/data';
 import { auth } from '@/lib/auth';
-import { dbClientHttp } from '@/lib/prisma';
 import { type ActionResponse } from '@/lib/types';
 
 export async function mergeCarts(userId: string): Promise<ActionResponse> {
-  const cookieStore = await cookies();
-  const sessionId = cookieStore.get('cartId')?.value;
+  const sessionId = await getCartCookie();
 
   if (!sessionId) {
     return { isSuccess: true };
   }
 
-  const guestCart = await dbClientHttp.cart.findUnique({
-    include: { items: { select: { createdAt: true, productId: true, quantity: true } } },
-    where: { sessionId },
-  });
+  const guestCart = await getGuestCartWithItems(sessionId);
 
   if (!guestCart) {
-    cookieStore.delete('cartId');
+    await deleteCartCookie();
     return { isSuccess: true };
   }
 
   if (guestCart.items.length === 0) {
-    await dbClientHttp.cart.delete({ where: { id: guestCart.id } });
-    cookieStore.delete('cartId');
+    await deleteCart(guestCart.id);
+    await deleteCartCookie();
     return { isSuccess: true };
   }
 
-  const userCart = await dbClientHttp.cart.upsert({
-    create: { userId },
-    include: { items: { select: { createdAt: true, productId: true, quantity: true } } },
-    update: {},
-    where: { userId },
-  });
+  const userCart = await createOrGetUserCartWithItems(userId);
+  const mergedCartItems = Object.values(
+    [...userCart.items, ...guestCart.items].reduce<
+      Record<CartItem['productId'], Omit<CartItem, 'id'>>
+    >((items, item) => {
+      const existingItem = items[item.product.id];
+      const clampedQuantity = Math.min(
+        item.quantity + (existingItem?.quantity ?? 0),
+        item.product.stock,
+      );
+      const earliestCreatedAt =
+        existingItem && existingItem.createdAt < item.createdAt ?
+          existingItem.createdAt
+        : item.createdAt;
 
-  const mergedItems = [...userCart.items, ...guestCart.items].reduce<
-    Record<CartItem['productId'], Pick<CartItem, 'quantity' | 'createdAt'>>
-  >((items, item) => {
-    const existingItem = items[item.productId];
-
-    if (!existingItem) {
       return {
         ...items,
-        [item.productId]: {
-          createdAt: item.createdAt,
-          quantity: item.quantity,
+        [item.product.id]: {
+          cartId: userCart.id,
+          createdAt: earliestCreatedAt,
+          productId: item.product.id,
+          quantity: clampedQuantity,
         },
       };
-    }
-
-    const earliestCreatedAt =
-      existingItem.createdAt < item.createdAt ? existingItem.createdAt : item.createdAt;
-
-    return {
-      ...items,
-      [item.productId]: {
-        createdAt: earliestCreatedAt,
-        quantity: existingItem.quantity + item.quantity,
-      },
-    };
-  }, {});
-
-  const productIds = Object.keys(mergedItems).map(Number);
-  const products = await dbClientHttp.product.findMany({
-    select: { id: true, stock: true },
-    where: { id: { in: productIds } },
-  });
-
-  const productsStock = products.reduce<Record<CartItem['productId'], CartItem['quantity']>>(
-    (stock, product) => {
-      stock[product.id] = product.stock;
-      return stock;
-    },
-    {},
+    }, {}),
   );
 
-  const mergedItemsSql = Object.entries(mergedItems).map(([productId, { createdAt, quantity }]) => {
-    const stock = productsStock[Number(productId)] ?? 0;
-    const clampedQuantity = Math.min(quantity, stock);
-
-    return Prisma.sql`(${userCart.id}, ${productId}, ${clampedQuantity}, ${createdAt})`;
-  });
-
-  // bulk upsert cart items
-  await dbClientHttp.$executeRaw`
-    INSERT INTO "CartItem" ("cartId", "productId", "quantity", "createdAt")
-    VALUES ${Prisma.join(mergedItemsSql)}
-    ON CONFLICT ("cartId", "productId")
-    DO UPDATE SET "quantity" = EXCLUDED."quantity", "createdAt" = EXCLUDED."createdAt";
-  `;
-
-  await dbClientHttp.cart.delete({ where: { id: guestCart.id } });
-  cookieStore.delete('cartId');
+  await upsertCartItems(mergedCartItems);
+  await deleteCart(guestCart.id);
+  await deleteCartCookie();
   revalidatePath('/');
 
   return { isSuccess: true };
@@ -108,18 +80,12 @@ export async function setCartItem(productId: number, quantity: number): Promise<
   const cartId: Cart['id'] = (await getCartId()) ?? (await createCart());
 
   if (quantity <= 0) {
-    await dbClientHttp.cartItem.delete({
-      where: { cartId_productId: { cartId, productId } },
-    });
-
+    await deleteCartItem(cartId, productId);
     revalidatePath('/');
     return { isSuccess: true };
   }
 
-  const product = await dbClientHttp.product.findUnique({
-    select: { stock: true },
-    where: { id: productId },
-  });
+  const product = await getProductStock(productId);
 
   if (!product) {
     return { isSuccess: false, message: 'Product not found' };
@@ -129,13 +95,7 @@ export async function setCartItem(productId: number, quantity: number): Promise<
     return { isSuccess: false, message: 'Not enough stock available' };
   }
 
-  await dbClientHttp.cartItem.upsert({
-    create: { cartId, productId, quantity },
-    select: { id: true },
-    update: { quantity },
-    where: { cartId_productId: { cartId, productId } },
-  });
-
+  await upsertCartItem(cartId, productId, quantity);
   revalidatePath('/');
   return { isSuccess: true };
 }
@@ -145,32 +105,13 @@ async function createCart(): Promise<Cart['id']> {
   const userId = session?.user.id;
 
   if (userId) {
-    return await createUserCart(userId);
+    const cartId = await createUserCart(userId);
+    return cartId;
   }
 
-  return await createGuestCart();
-}
-
-async function createGuestCart(): Promise<Cart['id']> {
   const sessionId = randomUUID();
-  const cookieStore = await cookies();
-  const newCart = await dbClientHttp.cart.create({
-    data: { sessionId },
-  });
+  const cartId = await createGuestCart(sessionId);
+  await setCartCookie(sessionId);
 
-  cookieStore.set('cartId', sessionId, {
-    httpOnly: true,
-    sameSite: 'strict',
-    secure: true,
-  });
-
-  return newCart.id;
-}
-
-async function createUserCart(userId: Cart['userId']): Promise<Cart['id']> {
-  const newCart = await dbClientHttp.cart.create({
-    data: { userId },
-  });
-
-  return newCart.id;
+  return cartId;
 }
