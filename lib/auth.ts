9ffbb2f8e5c257f1e35dcd3f66d16addd6b841bf
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-09-30 23:59:33 +0200

    cache auth util functions

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
 import { nextCookies } from 'better-auth/next-js';
 import { headers } from 'next/headers';
+import { cache } from 'react';
 
 import { dbPool } from './db';
 
@@ -12,12 +13,16 @@ export const auth = betterAuth({
   plugins: [nextCookies()],
 });
 
+const getSessionData = cache(async () => {
+  return auth.api.getSession({ headers: await headers() });
+});
+
 export async function getCurrentUser() {
-  const session = await auth.api.getSession({ headers: await headers() });
+  const session = await getSessionData();
   return session?.user ?? null;
 }
 
 export async function getSession() {
-  const session = await auth.api.getSession({ headers: await headers() });
+  const session = await getSessionData();
   return session?.session ?? null;
 }
