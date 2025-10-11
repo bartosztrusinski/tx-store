4ec29897ae3a8317b0fa2a37dacb706637650a06
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-10-11 18:45:47 +0200

    remove unused dal error

--- lib/dal.ts
+++ lib/dal.ts
@@ -1,11 +1,10 @@
 import { getCurrentUser } from '@/lib/auth';
 
-type DalErrorType = 'unauthenticated' | 'unauthorized' | 'unknown';
+type DalErrorType = 'unauthenticated' | 'unauthorized';
 
 const defaultMessage: Record<DalErrorType, string> = {
   unauthenticated: 'You must be logged in to perform this action',
   unauthorized: 'You are not authorized to perform this action',
-  unknown: 'An unknown error occurred. Please try again',
 };
 
 export class DalError extends Error {
