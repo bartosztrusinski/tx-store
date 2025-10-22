cc932b7388707782606be6f29e09ad53d09c0992
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-10-22 17:45:14 +0200

    create component for search bar

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,9 +1,6 @@
-import { Search } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
 import { APP_NAME } from '@/lib/constants';
 
 import { CategoryDrawer } from './category-drawer';
@@ -13,7 +10,7 @@ export function Header() {
   return (
     <header className='sticky top-0 z-50 border-b border-transparent bg-background shadow dark:border-border'>
       <div className='wrapper flex-between gap-3 px-2 sm:gap-4 sm:px-4'>
-        <section className='flex-center shrink-0 gap-3 md:gap-6'>
+        <div className='flex-center shrink-0 gap-3 md:gap-6'>
           <CategoryDrawer />
           <Link className='flex-center gap-1' href='/'>
             <Image
@@ -23,18 +20,9 @@ export function Header() {
               src='/images/logo.svg'
               width={32}
             />
-            <p className='heading-1 hidden md:block'>Store</p>
+            <span className='heading-1'>Store</span>
           </Link>
-        </section>
-        <section>
-          <form className='flex-center gap-1 sm:gap-2'>
-            <Input placeholder='Search' type='search' />
-            <Button className='shrink-0' size='icon' type='submit'>
-              <Search />
-              <span className='sr-only'>Search</span>
-            </Button>
-          </form>
-        </section>
+        </div>
         <HeaderActions />
       </div>
     </header>
---
+++ components/header/search-bar.tsx
@@ -0,0 +1,16 @@
+import { Search } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+
+export function SearchBar() {
+  return (
+    <form className='flex-center gap-1 sm:gap-2'>
+      <Input placeholder='Search' type='search' />
+      <Button className='shrink-0' size='icon' type='submit'>
+        <Search />
+        <span className='sr-only'>Search</span>
+      </Button>
+    </form>
+  );
+}
