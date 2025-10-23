eedef665a0529a95a8050383f6bb579e2d252be5
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-10-23 23:59:59 +0200

    set up header for mobile

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,30 +1,17 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+import { SearchBar } from '@/components/header/search-bar';
 import { APP_NAME } from '@/lib/constants';
-
-import { CategoryDrawer } from './category-drawer';
-import { HeaderActions } from './header-actions';
 
 export function Header() {
   return (
-    <header className='sticky top-0 z-50 border-b border-transparent bg-background shadow dark:border-border'>
-      <div className='wrapper flex-between gap-3 px-2 sm:gap-4 sm:px-4'>
-        <div className='flex-center shrink-0 gap-3 md:gap-6'>
-          <CategoryDrawer />
-          <Link className='flex-center gap-1' href='/'>
-            <Image
-              alt={`${APP_NAME} logo`}
-              height={32}
-              priority
-              src='/images/logo.svg'
-              width={32}
-            />
-            <span className='heading-1'>Store</span>
-          </Link>
-        </div>
-        <HeaderActions />
-      </div>
+    <header className='flex-between sticky top-0 z-10 gap-3 border-b border-transparent bg-background px-4 py-3 shadow dark:border-border'>
+      <Link className='flex-center shrink-0 gap-1' href='/'>
+        <Image alt={`${APP_NAME} logo`} height={32} priority src='/images/logo.svg' width={32} />
+        <span className='heading-1'>Store</span>
+      </Link>
+      <SearchBar />
     </header>
   );
 }
--- components/header/search-bar.tsx
+++ components/header/search-bar.tsx
@@ -6,7 +6,7 @@ import { Input } from '@/components/ui/input';
 export function SearchBar() {
   return (
     <form className='flex-center gap-1 sm:gap-2'>
-      <Input placeholder='Search' type='search' />
+      <Input className='text-sm' name='query' placeholder='Search' type='search' />
       <Button className='shrink-0' size='icon' type='submit'>
         <Search />
         <span className='sr-only'>Search</span>
