d30e10d7e470b99bd681ff22961eb18cdf33887a
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-10-31 18:05:56 +0100

    improve product price component

--- features/product/components/product-price.tsx
+++ features/product/components/product-price.tsx
@@ -1,35 +1,20 @@
-import { cn } from '@/lib/utils/cn';
+import { type ComponentProps } from 'react';
 
-type Props = {
+type Props = ComponentProps<'span'> & {
   price: number;
-  size?: 'default' | 'lg' | 'sm';
 };
 
-export function ProductPrice({ price, size = 'default' }: Props) {
+export function ProductPrice({ price, ...props }: Props) {
   const formattedPrice = new Intl.NumberFormat('en-US', {
     currency: 'USD',
     style: 'currency',
   }).formatToParts(price);
 
   return (
-    <span className={cn(size === 'sm' && 'text-sm', size === 'lg' && 'text-lg')}>
-      {formattedPrice.map(({ type, value }, index) =>
-        type === 'integer' ?
-          <span
-            className={cn(
-              'font-medium',
-              size === 'sm' && 'text-base lg:text-lg',
-              size === 'default' && 'text-lg lg:text-xl',
-              size === 'lg' && 'text-xl lg:text-2xl',
-            )}
-            key={index}
-          >
-            {value}
-          </span>
-        : type === 'decimal' || type === 'fraction' || (type === 'currency' && index === 0) ?
-          <sup key={index}>{value}</sup>
-        : value,
-      )}
+    <span {...props}>
+      {formattedPrice.map(({ value }, index) => (
+        <span key={index}>{value}</span>
+      ))}
     </span>
   );
 }
